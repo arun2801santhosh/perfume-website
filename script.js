e4dfaa927bb8941f786e39228523be81fc1501f9
arun2801santhosh <arunchandranmanikumar@gmail.com>
2026-06-16 22:57:15 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Perfume Website Loaded");
+let cart = [];
+
+let buttons = document.querySelectorAll(".add-to-cart");
+
+buttons.forEach(button => {
+  button.addEventListener("click", function () {
+    
+    let name = this.getAttribute("data-name");
+    let price = this.getAttribute("data-price");
+
+    let product = {
+      name: name,
+      price: price
+    };
+
+    cart.push(product);
+
+    console.log("Added to cart:", product);
+    console.log("Current Cart:", cart);
+
+    alert(name + " added to cart!");
+  });
+});
